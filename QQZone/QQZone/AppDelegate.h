1b7e87ce2b7c093ffe7f7abda1587df2b48dc45d
BJT <BJT@192.168.1.104>
2017-06-11 23:35:54 +0800

    初始化 QQ 空间 Demo

---
+++ QQZone/QQZone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQZone
+//
+//  Created by BJT on 17/6/11.
+//  Copyright © 2017年 BJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQZone/QQZone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQZone
+//
+//  Created by BJT on 17/6/11.
+//  Copyright © 2017年 BJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
